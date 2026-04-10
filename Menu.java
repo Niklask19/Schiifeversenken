9a434df7a2a77fe5017263303bd58037c7295d2f
Niklask19 <giraffewilly67@gmail.com>
2026-04-10 19:28:18 +0200

    Initial commit

---
+++ Menu.java
@@ -0,0 +1,35 @@
+package battleship;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static String menu() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the coordinates of the ship:");
+        return sc.nextLine();
+    }
+
+    public static Coordinates[] splitCoordinates(String coordinates) {
+        Coordinates c1 = new Coordinates();
+        Coordinates c2 = new Coordinates();
+        String[] array = coordinates.split(" ");
+
+        char row1 = array[0].charAt(0);
+        int column1 = Integer.parseInt(array[0].substring(1));
+        c1.setRow(row1);
+        c1.setCol(column1);
+
+        char row2 = array[1].charAt(0);
+        int column2 = Integer.parseInt(array[1].substring(1));
+        c2.setRow(row2);
+        c2.setCol(column2);
+
+        return new Coordinates[]{c1, c2};
+    }
+
+    public static void checkInput(String input) {
+
+    }
+
+}
