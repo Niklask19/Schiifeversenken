dbf7dcb2a2e993432f153f316b9bd5b4c4cbd2e6
Niklask19 <giraffewilly67@gmail.com>
2026-04-15 19:42:57 +0200

    added getLength and reworked control flow

--- Coordinates.java
+++ Coordinates.java
@@ -18,20 +18,53 @@ public class Coordinates {
 
     public boolean checkCoordinates(char row, int column) {
 
-        if(column > 10) {
-            return false;
+        if (column > 10) {
+            return false;  //check if row is above 10
         }
         String forbiddenLetters = "KLMNOPQRSTUVWXYZ";
 
-        for (int i = 0; i < forbiddenLetters.length(); i++) {
+        for (int i = 0; i < forbiddenLetters.length(); i++) { //check if coordinate contains letter A-J
             if (row == forbiddenLetters.charAt(i)) {
                 return false;
             }
         }
 
-
-
-
+        this.setRow(row);
+        this.setCol(column);
         return true;
     }
+
+    public boolean sortOutDiagonals(Coordinates x) {
+        if (this.row != x.row && this.col != x.col) {
+            this.col = 0;
+            this.row = 0;
+            x.row = 0;
+            x.col = 0;
+            return false;
+        }
+        return true;
+    }
+
+    public int getLength(Coordinates x) {
+        int count = 0;
+
+        if (this.row == x.row) {
+            if (this.col > x.col) {
+                int tempCol = x.col;
+                while (this.col >= tempCol) {
+                    tempCol++;
+                    count++;
+                }
+            }
+            if (this.col < x.col) {
+                int tempCol = this.col;
+                while(x.col >= tempCol) {
+                    tempCol++;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
--- Menu.java
+++ Menu.java
@@ -6,33 +6,45 @@ public class Menu {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the coordinates of the ship:");
-        return sc.nextLine();
+        String input = sc.nextLine();
+        return input;
     }
 
-    public static Coordinates[] splitCoordinates(String coordinates) {
-        Coordinates c1 = new Coordinates();
-        Coordinates c2 = new Coordinates();
+    public static void splitCoordinates(String coordinates, Coordinates[] cArray) {
+
         String[] array = coordinates.split(" ");
 
         char row1 = array[0].charAt(0);
         int column1 = Integer.parseInt(array[0].substring(1));
-        boolean quit1 = c1.checkCoordinates(row1, column1);
+        boolean quit1 = cArray[0].checkCoordinates(row1, column1);
 
-        if(!quit1) {
+        if (!quit1) {
             System.out.println("Error!");
-            return new Coordinates[]{c1, c2};
+            return;
         }
 
         char row2 = array[1].charAt(0);
         int column2 = Integer.parseInt(array[1].substring(1));
-        boolean quit2 = c2.checkCoordinates(row2, column2);
+        boolean quit2 = cArray[1].checkCoordinates(row2, column2);
 
         if (!quit2) {
             System.out.println("Error!");
-            return new Coordinates[]{c1, c2};
+            return;
         }
-        return new Coordinates[]{c1, c2};
+
+        boolean quit3 = cArray[0].sortOutDiagonals(cArray[1]);
+
+        if (!quit3) {
+            System.out.println("Error!");
+            return;
+        }
+
+        outputLengthAndParts(cArray);
     }
 
+    public static void outputLengthAndParts(Coordinates[] cArray) {
+        int length = cArray[0].getLength(cArray[1]);
+        System.out.println("Length: " + length);
+    }
 
 }
